7d8c74b10492ac0f51a88743c927e7346f4743b1
two-ticks <adityasiddheshwar.adisid@gmail.com>
2021-06-06 17:09:33 +0530

    createText tests

--- src/lib/Scene/add.ts
+++ src/lib/Scene/add.ts
@@ -1,4 +1,5 @@
 import { TeX } from '../MObject/TeX';
+import {Text} from '../MObject/Text';
 import * as CONFIG from '../config.js';
 
 export function add(_object: any) {
@@ -13,13 +14,24 @@ export function add(_object: any) {
     const g = _object.writeTexElement.elt.querySelectorAll('g');
     g[0].setAttribute('stroke-width', `${CONFIG.PLAY.STROKE_WIDTH}`);
     //console.log(CONFIG.PLAY.STROKE_WIDTH);
-    
-    //TODO : clean 
+
+    //TODO : clean
     //g[0].setAttribute('fill', 'none');
     // const pathEls = _object.writeTexElement.elt.querySelectorAll('path'); //nodelist
 
     //   for (var i = 0; i < pathEls.length; i++) {
     //     var pathEl = pathEls[i];
     //   }
+  } else if (_object instanceof Text) {
+    const sentence = _object.sentence;
+    const x = _object.x;
+    const y = _object.y;
+    _object.writeTextElement = createElement(
+      'h1',
+      sentence.replace(/\S/g, "<span class='letter'>$&</span>")
+    );
+    _object.writeTextElement.position(_object.x, _object.x);
+    _object.writeTextElement.style('font-size', `${_object.size}px`);
+    _object.writeTextElement.style('opacity', '0'); //to hide text at initialisation
   }
 }
--- src/test/sample.test.ts
+++ src/test/sample.test.ts
@@ -6,25 +6,25 @@ it('sample test', () => {
   //expect(add(2, 3)).toBe(5);
 });
 
-it('dummy test', () => {
-  const arg1 = new TeX('abc');
-  const arg2 = 234;
-  const arg3 = 123;
+// it('dummy test', () => {
+//   const arg1 = new TeX('abc');
+//   const arg2 = 234;
+//   const arg3 = 123;
 
-  const testSomeFunction = jest.fn((arg1, arg2, arg3) => {
-    shift(arg1, arg2, arg3);
-  });
-  testSomeFunction(arg1, arg2, arg3);
-  expect(testSomeFunction).toHaveBeenCalledWith(arg1, arg2, arg3);
-});
+//   const testSomeFunction = jest.fn((arg1, arg2, arg3) => {
+//     shift(arg1, arg2, arg3);
+//   });
+//   testSomeFunction(arg1, arg2, arg3);
+//   expect(testSomeFunction).toHaveBeenCalledWith(arg1, arg2, arg3);
+// });
 
-it.todo('shift should not accept the wrong argument type');
+// it.todo('shift should not accept the wrong argument type');
 
-test("load test",()=>{
-const load1 = 10;
-const load2 = 20;
-expect(load1+load2).toBeLessThan(160);
-});
+// test("load test",()=>{
+// const load1 = 10;
+// const load2 = 20;
+// expect(load1+load2).toBeLessThan(160);
+// });
 
 //expect(...).toHaveLength(4);
 //expect(...).toHaveBeenCalledWith('args1',agrs2,args4);
