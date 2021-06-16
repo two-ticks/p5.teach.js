727e9010181b68fb414a36946dbe3ad95b812b98
two-ticks <adityasiddheshwar.adisid@gmail.com>
2021-06-16 21:51:36 +0530

    test: mocks for play and add

--- src/test/Scene/add.test.ts
+++ src/test/Scene/add.test.ts
@@ -2,16 +2,26 @@
  * @jest-environment jsdom
  */
 
-import { play } from '../../lib/Scene/play';
-import { Text, createText } from '../../lib/MObject/Text';
-import p5 from 'p5';
+import module = require('p5');
+import * as text from '../../lib/MObject/Text';
+import * as Add from '../../lib/Scene/add';
+import { createText, Text } from '../../lib/MObject/Text';
+import { add } from '../../lib/Scene/add';
 global.color = require('color');
 
-it.todo('shift should not accept the wrong argument type');
-
-it.skip('check if play is called with an object', () => {
-  //Text.mockFn();
-  const fakeText = createText('fake text');
-
-  console.log(play(fakeText, 'write'));
+it('check if add is called with an object of type Text', () => {
+  jest.spyOn(Add, 'add').mockImplementation(() => {});
+  jest.spyOn(text, 'createText').mockImplementation(
+    (...args: any[]) =>
+      new Text({
+        x: 10,
+        y: 10,
+        _text: 'text',
+        _size: 28
+      })
+  );
+  let fakeText = createText('text');
+  expect(createText).toHaveBeenCalledWith('text');
+  add(fakeText);
+  expect(add).toHaveBeenCalledWith(fakeText);
 });
--- src/test/Scene/play.test.ts
+++ src/test/Scene/play.test.ts
@@ -3,33 +3,25 @@
  */
 
 import module = require('p5');
-import { Text, createText } from '../../lib/MObject/Text';
+import * as text from '../../lib/MObject/Text';
+import * as Play from '../../lib/Scene/play';
+import { createText, Text } from '../../lib/MObject/Text';
 import { play } from '../../lib/Scene/play';
-
 global.color = require('color');
 
-it('play must be running', () => {
-  const mockFn = jest.fn().mockReturnValue({
-    _fill: 'black',
-    sentence: 'text',
-    sizePx: 28,
-    x: 10,
-    y: 10
-  });
-  expect(mockFn()).toEqual({
-    _fill: 'black',
-    sentence: 'text',
-    sizePx: 28,
-    x: 10,
-    y: 10
-  });
-});
-
-it.skip('check if play is called with an object', () => {
-  console.log(text('text', 20, 20));
-
-  //Text.mockFn();
-  const fakeText = createText('fake text');
-
-  console.log(play(fakeText, 'write'));
+it('check if play is called with an object of type Text', () => {
+  jest.spyOn(Play, 'play').mockImplementation(() => {});
+  jest.spyOn(text, 'createText').mockImplementation(
+    (...args: any[]) =>
+      new Text({
+        x: 10,
+        y: 10,
+        _text: 'text',
+        _size: 28
+      })
+  );
+  let fakeText = createText('text');
+  expect(createText).toHaveBeenCalledWith('text');
+  play(fakeText);
+  expect(play).toHaveBeenCalledWith(fakeText);
 });
