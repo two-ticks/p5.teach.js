0e7ff246248c4ea53e8a51499cad0ef9294f9ecb
two-ticks <adityasiddheshwar.adisid@gmail.com>
2021-06-24 15:30:39 +0530

    feat : overflow hidden

--- src/lib/Scene/add.ts
+++ src/lib/Scene/add.ts
@@ -1,5 +1,6 @@
 import { TeX } from '../MObject/TeX';
 import { Text } from '../MObject/Text';
+import { sceneContainer } from './scene';
 
 export function add(object: Text | TeX) {
   if (false) {
@@ -11,10 +12,13 @@ export function add(object: Text | TeX) {
       'div',
       sentence.replace(/\S/g, "<span class='letter'>$&</span>")
     );
+    object.writeTextElement.style('white-space', ' nowrap');
     object.writeTextElement.position(object.x, object.y);
     object.writeTextElement.style('font-size', `${object._size}px`);
     object.writeTextElement.style('color', `${object.fillColor}`);
     //object.writeTextElement.style('text-stroke-width', `${object._strokeWidth}`); //TODO : not working without -webkit
     object.writeTextElement.style('opacity', '0'); //to hide text at initialisation
+    object.writeTextElement.remove();
+    sceneContainer.appendChild(object.writeTextElement.elt);
   }
 }
